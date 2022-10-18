c5df6d244901de98025a1802c3619da78c8b3f61
sindisiwe <sindisiwemashinini9@gmail.com>
2022-10-18 14:42:13 -0700

    printf

---
+++ myprintf/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
+
+/**
+ * void Prototypes()
+ * int Prototypes()
+ * char Prototypes()
+ */
+
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
